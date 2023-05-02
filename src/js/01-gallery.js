ac2c147d165ef468f7dce6b91a5caeb3807f3f13
ViktorKushnir <kushnirvv111@gmail.com>
2023-05-02 20:10:58 +0100

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+const galleryEl = document.querySelector(".gallery");
+
+function galleryCardMurkup(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `
+      <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+      </li>
+    `;
+    })
+    .join("");
+}
+
+const cardsMarcup = galleryCardMurkup(galleryItems);
+galleryEl.innerHTML = cardsMarcup;
+
+new SimpleLightbox(".gallery a", {
+  captionsData: `alt`,
+  captionDelay: 250,
+});
 console.log(galleryItems);
