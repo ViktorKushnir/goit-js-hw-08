de9d9dd8cc132e210348171a70d6812aa943d865
ViktorKushnir <kushnirvv111@gmail.com>
2023-05-02 21:00:18 +0100

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
 
+console.log(Player);
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const STORAGE_KEY = "videoplayer-current-time";
+
+function fillLocalStorage (data) {
+    localStorage.setItem(STORAGE_KEY, data.seconds)
+};
+
+player.on('timeupdate', throttle(fillLocalStorage, 1000));
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(STORAGE_KEY)));
