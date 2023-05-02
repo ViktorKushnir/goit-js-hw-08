f4c413392e3b433af4555f471a97eaa684d7d253
ViktorKushnir <kushnirvv111@gmail.com>
2023-05-02 22:37:48 +0100

    3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from "lodash.throttle";
 
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+const userInfo = {};
+populateSettings();
+
+form.addEventListener('input',throttle(onInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+function onInput(evt) {
+    // evt.preventDefault();
+    userInfo[evt.target.name] = evt.target.value;
+    
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(userInfo));
+}
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    console.log(userInfo);
+    evt.target.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+function populateSettings() {
+    const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+    
+    if (savedData) {
+        const { email, message } = savedData;
+        form.email.value = email;
+        form.message.value = message;
+        console.log(savedData);
+    }
+}
