3279124e802d4056673d55a972efe3fee474a8d2
ViktorKushnir <kushnirvv111@gmail.com>
2023-05-05 20:50:20 +0100

    3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,24 +1,42 @@
 import throttle from "lodash.throttle";
 
 const form = document.querySelector('.feedback-form');
+const formEmail = document.querySelector('form input');
+const formMessage = document.querySelector('form textarea');
 const STORAGE_KEY = 'feedback-form-state';
-const userInfo = {};
+let userInfo = {
+    email: '',
+    message: '',
+};
+
 populateSettings();
+
+
 
 form.addEventListener('input',throttle(onInput, 500));
 form.addEventListener('submit', onFormSubmit);
 
-function onInput(evt) {
-     userInfo[evt.target.name] = evt.target.value;
+function onInput(e) {
+     userInfo[e.target.name] = e.target.value;
     
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(userInfo));
+    localStorage.setItem(STORAGE_KEY,
+        JSON.stringify({...userInfo,
+        email: formEmail.value,
+        message: formMessage.value}));
 }
 
-function onFormSubmit(evt) {
-    evt.preventDefault();
-    console.log(userInfo);
-    evt.target.reset();
-    localStorage.removeItem(STORAGE_KEY);
+function onFormSubmit(e) {
+    e.preventDefault();
+    if (formEmail.value && formMessage.value){
+        form.reset();
+        localStorage.removeItem(STORAGE_KEY);
+        console.log(userInfo);
+        userInfo.email = '';
+        userInfo.message = '';             
+    } else {
+        alert("Всі поля форми мають бути заповнені!")
+    }
+   
 }
 
 function populateSettings() {
@@ -30,5 +48,9 @@ function populateSettings() {
         form.email.value = email;
         form.message.value = message;
         console.log(savedData);
+    } else {
+        form.email.value = '';
+        form.message.value = '';
+
     }
 }
